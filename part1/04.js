3c01ca04bcc36b53360819eef935da2593c349e2
Snehakhairnr <131871701+Snehakhairnr@users.noreply.github.com>
2023-04-28 16:49:15 +0530

    Update 04.js

--- part1/04.js
+++ part1/04.js
@@ -1,6 +1,13 @@
 // let keyword 
 // declare variable with let keyword 
 
+//let keyword sirf ekbaar used kar sakte hai same name variable ke liye 
+//wrong code
+let firstName = "harshit";
+let firstName = "Mohit";
+console.log(firstName);
+
+//right code
 let firstName = "harshit";
 firstName = "Mohit";
 console.log(firstName);
